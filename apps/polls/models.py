75128b39ec65f8fa0c779a0385fa0fc01b1cbbcf
Eric Ndung'u <ericobain@gmail.com>
2022-01-03 17:56:11 +0300

    questions and choices

--- apps/polls/models.py
+++ apps/polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Question(models.Model):
+    """Poll question."""
+    question_text = models.CharField(_("Question text"), max_length=200)
+    pub_date = models.DateTimeField(_("Publish date"))
+
+
+class Choice(models.Model):
+    """Answer choices for a question."""
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(_("Choice text"), max_length=200)
+    votes = models.IntegerField(default=0)
